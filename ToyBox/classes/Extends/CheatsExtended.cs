f2bc8a2b287ed1ac7a3a13bfb4abe8f4724faef7
Nathanael Yusuf Tj <26967808+nathanaelytj@users.noreply.github.com>
2022-08-06 11:15:03 +0700

    Update CheatsExtended.cs
    Removed double buff that crash

--- ToyBox/classes/Extends/CheatsExtended.cs
+++ ToyBox/classes/Extends/CheatsExtended.cs
@@ -21,8 +21,8 @@ namespace ToyBox.classes.Extends {
                 BlueprintBuff[] buffArray =
                 {
                     Utilities.GetBlueprintByGuid<BlueprintBuff>("09b4b69169304474296484c74aa12027"), /* True Seeing */
+                    Utilities.GetBlueprintByGuid<BlueprintBuff>("0adecbf63b614e846bfe15c33f34507e"), /* See Invisibility */
                     Utilities.GetBlueprintByGuid<BlueprintBuff>("03464790f40c3c24aa684b57155f3280"), /* Haste */
-                    Utilities.GetBlueprintByGuid<BlueprintBuff>("9100128a9fd947b89344ad27f98833ff"), /* Angel Ward Divine Spell */
                     Utilities.GetBlueprintByGuid<BlueprintBuff>("87b8c6270ea85c743afc734dfe99afee"), /* Bless */
                     Utilities.GetBlueprintByGuid<BlueprintBuff>("0bd122cf77c3d6948929de45e18e4fed"), /* Bone Shield */
                     Utilities.GetBlueprintByGuid<BlueprintBuff>("35f3724d4e8877845af488d167cb8a89"), /* Mind Blank */
@@ -35,26 +35,17 @@ namespace ToyBox.classes.Extends {
                     Utilities.GetBlueprintByGuid<BlueprintBuff>("453201f110f49714fb1d713c1f7bc06d"), /* Resist Electricity */
                     Utilities.GetBlueprintByGuid<BlueprintBuff>("b9ce1623447547946b8c58efd069f7c4"), /* Resist Fire */
                     Utilities.GetBlueprintByGuid<BlueprintBuff>("63e2a6323c2bbff40a87fcd522778032"), /* Resist Sonic */
-                    Utilities.GetBlueprintByGuid<BlueprintBuff>("0adecbf63b614e846bfe15c33f34507e"), /* See Invisibility */
                     Utilities.GetBlueprintByGuid<BlueprintBuff>("50a77710a7c4914499d0254e76a808e5"), /* Spell Resistance */
                     Utilities.GetBlueprintByGuid<BlueprintBuff>("b175001b42b1a02479881b72fe132116"), /* Bull STR */
-                    Utilities.GetBlueprintByGuid<BlueprintBuff>("4814db563c105e64d948161162715661"), /* False Life */
 
                     /** EXPERIMENTAL **/
+                    Utilities.GetBlueprintByGuid<BlueprintBuff>("9100128a9fd947b89344ad27f98833ff"), /* Angel Ward Weakness Divine Spell */
                     Utilities.GetBlueprintByGuid<BlueprintBuff>("68d82c1249d3e4c47a69946b1eea0dd6"), /* Angel Ward Weakness */
-                    Utilities.GetBlueprintByGuid<BlueprintBuff>("8801f41b253263249a3d3a46cdf3b276"), /* Angel Ward Harm */
-                    Utilities.GetBlueprintByGuid<BlueprintBuff>("a00be2f66cc2ee34a847e19244b30239"), /* Angel Ward Disease */
-                    Utilities.GetBlueprintByGuid<BlueprintBuff>("384e8507f54fd3342ad9fd6101bc106a"), /* Angel Ward Unclean */
-                    Utilities.GetBlueprintByGuid<BlueprintBuff>("c5455f88975e16648b4abc47a28fa120"), /* Last Stand Effect */
 
                     Utilities.GetBlueprintByGuid<BlueprintBuff>("3db4a1f9ffa46e7469f817bced1a0df2"), /* Ascendant Summon */
                     Utilities.GetBlueprintByGuid<BlueprintBuff>("b8da3ec045ec04845a126948e1f4fc1a"), /* Heroism Greater */
 
-                    Utilities.GetBlueprintByGuid<BlueprintBuff>("07162c026878db04cabe4fc3edc90585"), /* Shaman Ward of Flame */
-                    Utilities.GetBlueprintByGuid<BlueprintBuff>("add62fda83dcbfb408ef2976e0271d2c"), /* Shaman Ward of Wind */
-
                     Utilities.GetBlueprintByGuid<BlueprintBuff>("256ae19b117c40868248154da12d17b8"), /* Aeon Corruption Protection */
-                    Utilities.GetBlueprintByGuid<BlueprintBuff>("5d97cd157d9f52441b6e5ab665d1b36f"), /* Azata Rest */
                     Utilities.GetBlueprintByGuid<BlueprintBuff>("444de30dc7a99144e852bd7dbaeddaad"), /* Azata Tree Strong */
                     Utilities.GetBlueprintByGuid<BlueprintBuff>("adcc8e0615fa4e37bd7e5854302203fe"), /* Trickster Double Damage */
                     Utilities.GetBlueprintByGuid<BlueprintBuff>("65ea55e5521d43409d102defd153757a"), /* Legend Rest */
@@ -74,10 +65,6 @@ namespace ToyBox.classes.Extends {
                     Utilities.GetBlueprintByGuid<BlueprintBuff>("e4a2e8fe03063104098d088f70ff1d62"), /* WIS */
 
                     /** Weapon Effect **/
-                    Utilities.GetBlueprintByGuid<BlueprintBuff>("2f2eb113da65b6b4fbf69e35c90afe02"), /* Chaos Buff */
-                    Utilities.GetBlueprintByGuid<BlueprintBuff>("418e078eb0eb6494ab65d124ab5eb7fe"), /* Evil Buff */
-                    Utilities.GetBlueprintByGuid<BlueprintBuff>("cbe785a099249164eb2204f553437e3d"), /* Good Buff */
-                    Utilities.GetBlueprintByGuid<BlueprintBuff>("57e74017b222b274790c5fa162855035"), /* Lawful Buff */
                     Utilities.GetBlueprintByGuid<BlueprintBuff>("5fe69b8d755a4bd18e4e67c5613f76af"), /* Slasing Buff */
                     Utilities.GetBlueprintByGuid<BlueprintBuff>("4bbff3a1c8894a46be2515029ea044b0"), /* Magic Damage Fighter Elemental */
                 };
